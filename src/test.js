aad7076caaa4a61f1519747856ddb6aeb9c0c6b1
Joakim Olsson <barbatron@gmail.com>
2020-04-10 17:55:09 +0000

    Add some console output

--- src/test.js
+++ src/test.js
@@ -1,6 +1,17 @@
-
 const { Gpio } = require('onoff');
 const led = new Gpio(17, 'out');
 const button = new Gpio(4, 'in', 'both');
 
-button.watch((err, value) => led.writeSync(value));
+button.watch((err, value) => {
+  console.log('button press', value); 
+  led.writeSync(value);
+});
+
+
+process.on('SIGINT', _ => {
+  led.unexport();
+  button.unexport();
+});
+
+console.log('Waiting');
+
